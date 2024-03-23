083ec5eebc6396e44d2565ff2b9bd2329b0f8339
Maksym Stoliarov <Windmill78max@gmail.com>
2024-03-23 18:24:16 +0100

    Leetcode P21 another solution

--- leetcode/easy/21. Merge Two Sorted Lists/main.go
+++ leetcode/easy/21. Merge Two Sorted Lists/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -25,10 +27,36 @@ func mergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 	}
 }
 
+func mergeTwoLists2(list1, list2 *ListNode) *ListNode {
+	result := new(ListNode)
+	head := result
+
+	for list1 != nil && list2 != nil {
+		if list1.Val <= list2.Val {
+			result.Next = list1
+			list1 = list1.Next
+		} else {
+			result.Next = list2
+			list2 = list2.Next
+		}
+		result = result.Next
+	}
+
+	if list1 == nil && list2 != nil {
+		result.Next = list2
+	}
+
+	if list2 == nil && list1 != nil {
+		result.Next = list1
+	}
+
+	return head.Next
+}
+
 func main() {
 	l1 := &ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}}
 	l2 := &ListNode{Val: 2, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3}}}
 
-	r := mergeTwoLists(l1, l2)
+	r := mergeTwoLists2(l1, l2)
 	fmt.Println(r)
 }
