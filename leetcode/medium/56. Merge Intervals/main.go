eb15aa24b877714fca8987c76cff133e20de39de
Maksym Stoliarov <Windmill78max@gmail.com>
2024-03-31 14:47:58 +0200

    Leetcode P56

--- leetcode/medium/56. Merge Intervals/main.go
+++ leetcode/medium/56. Merge Intervals/main.go
@@ -1,13 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func merge(intervals [][]int) [][]int {
-	return nil
+	if len(intervals) == 0 {
+		return nil
+	}
+
+	if len(intervals) == 1 {
+		return intervals
+	}
+
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	l := 0
+	m := false
+
+	for r := 1; r < len(intervals); r++ {
+		if intervals[l][1] >= intervals[r][0] {
+			intervals[l][0] = min(intervals[l][0], intervals[r][0])
+			intervals[l][1] = max(intervals[l][1], intervals[r][1])
+
+			intervals = append(intervals[:r], intervals[r+1:]...)
+			m = true
+		}
+
+		if m == true {
+			r--
+			m = false
+		} else {
+			l++
+		}
+	}
+
+	return intervals
 }
 
 func main() {
-	intervals := [][]int{{1, 4}, {3, 5}, {7, 9}}
+	var intervals [][]int
+	intervals = [][]int{{1, 4}, {3, 5}, {1, 9}, {9, 10}, {11, 12}, {11, 15}}
+
+	fmt.Println(intervals)
+
+	//intervals = [][]int{{1, 4}, {2, 3}}
+
+	//intervals = [][]int{{1, 4}, {0, 1}}
+
+	//intervals = [][]int{{0, 0}, {1, 4}}
 
 	res := merge(intervals)
 
